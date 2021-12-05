1a231e1bb7e62bc8b22cd49b31e78275498d1011
hxz <2826081307@qq.com>
2021-12-05 11:48:43 +0800

    首页

---
+++ CourseDesign/FormIndex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseDesign
+{
+    public partial class FormIndex : Form
+    {
+        private static FormIndex FrmIndex = null;
+        public FormIndex()
+        {
+            InitializeComponent();
+        }
+        public static FormIndex getSingle()
+        {
+            if(FrmIndex==null || FrmIndex.IsDisposed)
+            {
+                FrmIndex = new FormIndex();
+            }
+            return FrmIndex;
+        }
+    }
+}
