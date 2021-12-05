23074bd51671aa90eca9d93c1062860f03fde9ed
hxz <2826081307@qq.com>
2021-12-05 11:49:16 +0800

    完成程序的大体布局

--- CourseDesign/FormMain.cs
+++ CourseDesign/FormMain.cs
@@ -15,14 +15,19 @@ namespace CourseDesign
     {
         private static FormMain singleMain = null; 
         FormLogin frmLogin = FormLogin.GetSingle();
+        FormIndex frmIndex;
+
         FormInfo frmInfo;
         //保存当前用户的手机号码
         string phone;
+        //保存用户id
+        string id;
         DBHelper helper = new DBHelper("mysql");
         IDataReader reader;
         string sql;
         string name;
-        string avatar = "pic1.jpg";
+        //设置默认头像
+        string avatar = "default.png";
         public FormMain()
         {
             InitializeComponent();
@@ -53,7 +58,8 @@ namespace CourseDesign
 
             //根据不同用户选择头像
             phone = FormLogin.userPhone;
-            sql = "select name, avatarName from user where phone='" + phone + "'";
+            id = FormLogin.userID;
+            sql = "select name, avatarName from user where id='" + id + "'";
             reader = helper.DataRead(sql);
             if (reader != null && reader.Read())
             {
@@ -84,9 +90,11 @@ namespace CourseDesign
             frmInfo.ChangeAvatar += new FormInfo.MyEvent(changeAvatar);
             frmInfo.MdiParent = this;
             frmInfo.TopLevel = false;
-            frmInfo.Dock = System.Windows.Forms.DockStyle.Fill;
-            panel.Controls.Add(frmInfo);
+            frmInfo.Dock = DockStyle.Fill;
             frmInfo.Show();
+            panel.Controls.Clear();
+            panel.Controls.Add(frmInfo);
+            
         }
 
         private void changeAvatar(string name)
@@ -95,6 +103,17 @@ namespace CourseDesign
             MessageBox.Show("为什么会执行这里啊？？");
             pbAvatar.BackgroundImage = Image.FromFile(FormRegister.avatarPath+"\\"+name);
         }
-        
+
+        private void mmuIndex_Click(object sender, EventArgs e)
+        {
+            frmIndex = FormIndex.getSingle();
+            frmIndex.MdiParent = this;
+            frmIndex.TopLevel = false;
+            frmIndex.Dock = DockStyle.Fill;
+            frmIndex.Show();
+            panel.Controls.Clear();
+            panel.Controls.Add(frmIndex);
+
+        }
     }
 }
